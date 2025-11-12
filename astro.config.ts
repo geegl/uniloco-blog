81809f366520824521a46553003187833043328c
geegl <hezhan188@gmail.com>
2025-11-13 00:03:21 +0800

    Update astro.config.ts
    resource to /blog/_astro/styles.css

--- astro.config.ts
+++ astro.config.ts
@@ -22,6 +22,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  base: '/blog', // resource to /blog/_astro/styles.css
   output: 'static',
 
   integrations: [
